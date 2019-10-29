ff521da7317817d73022da13fbe1313e7af37ac9
eyardley <eyardley27@gmail.com>
2019-10-29 12:38:40 -0700

    Initial Working Priority Implementation

--- src/main/java/edu/sfsu/ProcessScheduler.java
+++ src/main/java/edu/sfsu/ProcessScheduler.java
@@ -1,8 +1,7 @@
 package edu.sfsu;
 
 import com.google.common.base.Preconditions;
-import java.util.HashMap;
-import java.util.Map;
+import java.util.PriorityQueue;
 
 /**
  * Process Scheduler
@@ -13,7 +12,7 @@ import java.util.Map;
 class ProcessScheduler {
 
   private static final int RUN_CYCLES = 5; // Default number of cycles given to each process on a single run.
-  private final Map<Integer, SimulatedProcess> processControlBlock = new HashMap<>();
+  private final PriorityQueue<SimulatedProcess> processControlBlock = new PriorityQueue<>(5, SimulatedProcess::compareTo);
   private final CentralProcessingUnit cpu;
   private int lastAssignedProcessNumber = 0;
 
@@ -35,10 +34,8 @@ class ProcessScheduler {
    * instead.
    */
   void processDone(SimulatedProcess process) {
-    // TODO: replace this ROUND-ROBIN SOLUTION.
-    int currentRunningProcess = process.processNumber();
-    if (processControlBlock.containsKey(currentRunningProcess + 1)) {
-      SimulatedProcess nextProcess = processControlBlock.get(currentRunningProcess + 1);
+    if (processControlBlock.peek() != null) {
+      SimulatedProcess nextProcess = processControlBlock.poll();
       cpu.runProcess(this, nextProcess, RUN_CYCLES);
     }
   }
@@ -47,13 +44,11 @@ class ProcessScheduler {
    * Add a single process.
    */
   synchronized void addProcess(SimulatedProcess process) {
-    lastAssignedProcessNumber++;
-    process.setProcessNumber(lastAssignedProcessNumber);
+    process.setProcessNumber(++lastAssignedProcessNumber);
+    processControlBlock.add(process);
 
-    // Priority is ignored in this version.
-    processControlBlock.put(lastAssignedProcessNumber, process);
     if (cpu.isIdle()) {
-      cpu.runProcess(this, process, RUN_CYCLES);
+      cpu.runProcess(this, processControlBlock.poll(), RUN_CYCLES);
     }
   }
 
